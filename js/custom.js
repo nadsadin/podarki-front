fa29154ca6468c1c5fe223898a624dc45a8a1d2a
Nadsadin <leloushlampergh@gmail.com>
2019-05-25 15:34:51 +0300

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    //initialize swiper when document ready
+    var mySwiper = new Swiper ('.swiper-container', {
+        // Optional parameters
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+        },
+    })
+});
